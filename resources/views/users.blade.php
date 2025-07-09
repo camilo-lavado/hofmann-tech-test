63d99a9039783713fce3c3e1aea2f54e32327b55
Camilo Lavado <93832685+camilo-lavado@users.noreply.github.com>
2025-07-09 17:53:32 -0400

    Vista de prueba para fetching de datos.

---
+++ resources/views/users.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Prueba Laravel - Usuarios</title>
+</head>
+<body>
+    <h1>Vista conectada correctamente</h1>
+
+    <h2>Users (ListTableUsers)</h2>
+    <pre>{{ print_r($users, true) }}</pre>
+
+    <h2>Codes (GetUsers)</h2>
+    <pre>{{ print_r($codes, true) }}</pre>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\UserController;
+
+Route::get('/', [UserController::class, 'index']);
+Route::post('/send', [UserController::class, 'send'])->name('send-user');
